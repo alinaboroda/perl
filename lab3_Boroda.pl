ef312bc79b261c5abe560c86a30edabeb166daa3
alinaboroda <alichka_18@i.ua>
2016-05-18 16:47:51 +0300

    All done, but not exceptions

--- lab3_Boroda.pl
+++ lab3_Boroda.pl
@@ -16,14 +16,16 @@ while (<$fh>) {
 
 #print $_ . '  ' . $port_serv{$_} . "\n" for ( keys (%port_serv) );
 
-if ( $ARGV[0] and $ARGV[1]) {
-	print "YES";
+if ( int($ARGV[0]) > 0 and int($ARGV[1]) > 0 ) {
+	for (my $i = $ARGV[0]; $i <= $ARGV[1]; $i++) {
+		print $i . " - " . $port_serv{$i} . "\n" if ( $port_serv{$i} );
+}
 
-} elsif ( $ARGV[0] ) {
+} elsif ( int($ARGV[0]) > 0 ) {
 	print $ARGV[0] . " - " . $port_serv{$ARGV[0]} . "\n";
 
 } else {
-		print "You don't enter number of port \n";
+		print "You don't enter number of port or You enter not positive number \n";
 
 }
 
