a43cadacb629a3906a904db9537d9a53905f37fe
alinaboroda <alichka_18@i.ua>
2016-05-20 16:33:32 +0300

    Add ping, outputFile

--- lab4_Boroda.pl
+++ lab4_Boroda.pl
@@ -1,28 +1,30 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $fileName;
-my $count;
-if (defined $ARGV[0]) {
-    $fileName = $ARGV[0];
-    if (defined $ARGV[1]) {
-        $count = $ARGV[1];
-        open (my $fh, "<", "$fileName") or die "Can't open the file.\n";
-        while (<$fh>) {
-            chomp;
-            next unless ($_);
-            system "ping -c $count $_";
-            
+my ($inputFile, $count, $outputFile) = @ARGV;
 
-        }
-    } else {
-        print "You don't enter count.\n";
+die "You don't enter a name of file.\n" unless (defined $inputFile);
+die "You don't enter a number of requests for each host.\n" unless (defined $count);
+
+my @ping;
+my $max_time;
+my $loss;
+my $ip;
+
+open (my $fh, "<", "$inputFile") or die "Can't open the file.\n";
+open (my $fo, ">>", "$outputFile") if (defined $outputFile);
+
+while (<$fh>) {
+    chomp;
+    next unless ($_);
+    for my $host (split ' ', $_) {
+        @ping = (`ping -c $count $host`)[0, -2, -1];
+        $ip = join (' ', (split ' ', $ping[0])[1, 2]);
+        $loss = (split ' ', $ping[1])[5];
+        $max_time = (split '/', (split ' ', $ping[2])[3])[2];
+        print "$ip: max_time = $max_time; $loss of loss;\n";
+        print $fo `date` . "$ip: max_time = $max_time; $loss of loss;\n" if (defined $outputFile);
+
+      
     }
-
-
-
-
-} else {
-    print "You don't enter fileName.\n";
 }
-#neskolko v stochke
